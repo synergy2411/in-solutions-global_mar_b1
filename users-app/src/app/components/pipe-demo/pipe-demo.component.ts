61e835c50e77dc44b8b8b9c8f8ff173ceb9af0cf
unknown <Sumit Khandelwal>
2021-03-17 17:05:26 +0530

    the notes app

--- users-app/src/app/components/pipe-demo/pipe-demo.component.ts
+++ users-app/src/app/components/pipe-demo/pipe-demo.component.ts
@@ -3,19 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pipe-demo',
   templateUrl: './pipe-demo.component.html',
-  styleUrls: ['./pipe-demo.component.css']
+  styleUrls: ['./pipe-demo.component.css'],
 })
 export class PipeDemoComponent implements OnInit {
-
   promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve("Here the data comes...")
+      resolve('Here the data comes...');
     }, 3000);
-  })
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
