f58340972d1820607a4d3e192f6b28020aa2fa66
unknown <Sumit Khandelwal>
2021-03-19 09:49:02 +0530

    confirm password validator added

--- users-app/src/app/components/register/register.component.ts
+++ users-app/src/app/components/register/register.component.ts
@@ -1,42 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
-  email = new FormControl('',[
-    Validators.required,
-    Validators.email
-  ]);
+  email = new FormControl('', [Validators.required, Validators.email]);
   password = new FormControl('', [
     Validators.required,
     Validators.minLength(6),
-    this.hasExclamation
+    this.hasExclamation,
+  ]);
+  cnfPassword = new FormControl('', [
+    Validators.required,
+    this.confirmPassword,
   ]);
 
-  hasExclamation(input : FormControl){
-    const hasExcl = input.value.indexOf("!") >=0
-    return hasExcl ? null : {hasExclamation : true}
+  confirmPassword(input : FormControl){
+    console.log(input);
+    if(input.root && input.root.value){
+      console.log("Password - ", input.root.value.password);
+      const isPasswordMatch = input.root.value.password === input.value;
+      return isPasswordMatch ? null : {passwordMatch : true}
+    }
   }
 
-  registerForm : FormGroup;
-
-  constructor(private fb : FormBuilder) { 
-     this.registerForm =  this.fb.group({
-      email : this.email,
-      password : this.password
-    })
+  hasExclamation(input: FormControl) {
+    const hasExcl = input.value.indexOf('!') >= 0;
+    return hasExcl ? null : { hasExclamation: true };
   }
 
-  onRegister(){
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      email: this.email,
+      password: this.password,
+      cnfPassword : this.cnfPassword
+    });
+  }
+
+  onRegister() {
     console.log(this.registerForm);
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
